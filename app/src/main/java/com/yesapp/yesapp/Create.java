ce257da59cdce0969cae14f107f91c366bc025aa
Rasheed menem <rasheed.menem1@gmail.com>
2018-07-08 13:13:24 +0300

    firebase reading not working

--- app/src/main/java/com/yesapp/yesapp/Create.java
+++ app/src/main/java/com/yesapp/yesapp/Create.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -16,6 +20,8 @@ public class Create extends AppCompatActivity {
 
     EditText messageText;
     DatabaseReference myRef;
+    TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +33,31 @@ public class Create extends AppCompatActivity {
 
     public void post(View view) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
+        myRef = database.getReference("Posts : ");
         writeNewUser(messageText.getText().toString(), messageText.getText().toString() );
+        text = (TextView) findViewById(R.id.textView2);
 
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                text.setText(post.message);
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
     }
 
     private void writeNewUser(String userId, String message ) {
         User user = new User(userId, message);
 
-        myRef.child("users").child(userId).setValue(message);
+        myRef.child("posts").child(userId).setValue(message);
     }
 }
--- app/src/main/java/com/yesapp/yesapp/MainActivity.java
+++ app/src/main/java/com/yesapp/yesapp/MainActivity.java
@@ -1,21 +1,40 @@
 package com.yesapp.yesapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference myRef;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
 
     }
 
     public void gotocreate(View view) {
         Intent i = new Intent(MainActivity.this, Create.class);
         startActivity(i);
+    }
+
+    public void Read(View view) {
+
+
     }
 }
