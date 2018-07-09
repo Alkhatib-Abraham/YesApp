1aa9f9f9bc6065cad90f125de43c90eaa0cda276
Abraham <alkhatib.abraham@gmail.com>
2018-07-09 17:54:17 +0200

    shared preference added sucsess

--- app/src/main/java/com/yesapp/yesapp/login.java
+++ app/src/main/java/com/yesapp/yesapp/login.java
@@ -1,6 +1,7 @@
 package com.yesapp.yesapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,22 +17,42 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class login extends AppCompatActivity {
+    //for saving the login data
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sp = getSharedPreferences("login",MODE_PRIVATE);
+
+        if(sp.getBoolean("logged",false)){
+            startActivity(new Intent(login.this, MainActivity.class));
+        }
     }
 
+
+    SharedPreferences sp;
+
+
+
     public void login(View view) {
+
+
+
         EditText email2 =    (EditText) findViewById(R.id.edit1);
         EditText password2 = (EditText) findViewById(R.id.edit2);
         String email3 = email2.getText().toString();
         String password3 = password2.getText().toString();
+        //to save the logindata
+        sp.edit().putString("name",email3).apply();
+        sp.edit().putString("password",password3).apply();
+
+
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email3, password3).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
               if(task.isSuccessful()){
+                  sp.edit().putBoolean("logged",true).apply();
 
                   startActivity(new Intent(login.this, MainActivity.class));
               }else{
