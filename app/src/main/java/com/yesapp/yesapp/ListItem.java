55506b5093649b92557a1f637d97fccaf1afc428
Abraham <alkhatib.abraham@gmail.com>
2018-07-19 20:03:10 +0200

    Some StyleChanges

--- app/src/main/java/com/yesapp/yesapp/ListItem.java
+++ app/src/main/java/com/yesapp/yesapp/ListItem.java
@@ -5,7 +5,10 @@ public class ListItem {
     public String Name;
     public String Desc;
     public String User;
+
+
     ListItem(String Name,String Desc,String User){
+
         this.Name=Name;
         this.Desc=Desc;
         this.User=User;
