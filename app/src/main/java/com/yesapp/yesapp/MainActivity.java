6e73de66affa6076f0da2f9450ba56b531d9b43f
Abraham <alkhatib.abraham@gmail.com>
2018-07-28 19:59:37 +0200

    Organizing and renaming the code phase 1

--- app/src/main/java/com/yesapp/yesapp/MainActivity.java
+++ app/src/main/java/com/yesapp/yesapp/MainActivity.java
@@ -1,12 +1,9 @@
 package com.yesapp.yesapp;
 
-import android.app.LauncherActivity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +13,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,18 +26,23 @@ import java.util.List;
 // This is to read data from a database and place it in a list view
 public class MainActivity extends AppCompatActivity {
 
-    ListView ls;
-    SwipeRefreshLayout swipeRefreshLayout;
-    private int onBackPressed = 0;
+    ListView mainScreenListView;           //to contain the posts
+    SwipeRefreshLayout swipeRefreshLayout; //to refresh
+    private int onBackPressed = 0;         //to track how many times back was pressed
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
-        Refresh();
-        ls = (ListView) findViewById(R.id.list);
 
-        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        refresh(); //to get the new content
+
+     //============================to open a screen when clicking on an Item========================
+        mainScreenListView = (ListView) findViewById(R.id.list);
+        mainScreenListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(MainActivity.this,PostView.class);
@@ -53,68 +52,52 @@ public class MainActivity extends AppCompatActivity {
                 TextView userName =(TextView) view.findViewById(R.id.txtuser);
                 TextView discr =(TextView) view.findViewById(R.id.textView14);
 
-
-                String city = cityName.getText().toString().trim();
-                String action = actionName.getText().toString().trim();
-                String user = userName.getText().toString().trim();
-                String disc = discr.getText().toString().trim();
-
-
-                intent.putExtra("city",city);
-                intent.putExtra("action",action);
-                intent.putExtra("user",user);
-                intent.putExtra("discreption",disc);
-
-
-
-
+                intent.putExtra("city",cityName.getText().toString().trim());
+                intent.putExtra("action",actionName.getText().toString().trim());
+                intent.putExtra("user",userName.getText().toString().trim());
+                intent.putExtra("discreption",discr.getText().toString().trim());
                 startActivity(intent);
 
             }
         });
+        //--------------------------------The End of the Listener-----------------------------------
 
 
 
+
+        //======================SwipeRefresh Layout Listener to track the refreshing================
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                Refresh();
-
+                refresh();
             }
         });
-
     }
-
-    @Override
-    protected void onResume() {
-
-        super.onResume();
-    }
-
-    //=====================================================================================================
-// this function triggeres when user presses the go to create button  to post posts in the database
-    public void gotocreate(View view) {
-        Intent i = new Intent(MainActivity.this, Create.class);
-        startActivity(i);
-    }
-
-//=====================================================================================================
+    //-------------------------End of the SwipeRefresh Layout Listener------------------------------
 
 
 
 
+
+
+    //================================to prevent unwanted exiting of the app========================
     @Override
     public void onBackPressed() {
-
         Toast.makeText(this, "Press Back again to Exit",Toast.LENGTH_SHORT).show();
         if(onBackPressed==1){
             onBackPressed =0;
             super.onBackPressed();
         }
         onBackPressed ++;
-
     }
-    public void Refresh(){
+    //----------------------------------------------------------------------------------------------
+
+
+
+
+    //===============the Void which loads new content and put it in the listView====================
+
+    public void refresh(){
         //necessary References
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference posts = database.getReference("posts");
@@ -139,8 +122,8 @@ public class MainActivity extends AppCompatActivity {
 
                         // transfers the ListArray into the ListView with the CustomAdapter
                         final MyCustomAdapter myadpter = new MyCustomAdapter(Items);
-                         ls = (ListView) findViewById(R.id.list);
-                        ls.setAdapter(myadpter);
+                        mainScreenListView = (ListView) findViewById(R.id.list);
+                        mainScreenListView.setAdapter(myadpter);
                     }
 
                 }}//end of the add On DataChange listener
@@ -150,40 +133,54 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }); //end of the Database.addValueEventListener
-      Toast.makeText(MainActivity.this,"Refreshed",Toast.LENGTH_SHORT).show();
-    swipeRefreshLayout.setRefreshing(false); //stop refreshing
+
+        Toast.makeText(MainActivity.this,"Refreshed",Toast.LENGTH_SHORT).show();
+        swipeRefreshLayout.setRefreshing(false); //stop refreshing
 
     }
+    //----------------------------------End of refresh()--------------------------------------------
 
-    public void goToSettings(View view) {
+
+
+    //=============================go to the Create Activity========================================
+    public void goToCreateActivity(View view) {
+        Intent i = new Intent(MainActivity.this, Create.class);
+        startActivity(i);
+    }
+    //----------------------------------------------------------------------------------------------
+
+
+    //================================Go to the Settings Activity===================================
+    public void goToSettingsActivity(View view) {
         Intent intent = new Intent(MainActivity.this,Settings.class);
         startActivity(intent);
     }
+    //----------------------------------------------------------------------------------------------
 
 
 
 
-    //==============================================================================================
 
+    //===========================The Custom Adapter Class===========================================
 
     class MyCustomAdapter extends BaseAdapter
         //this class defines how the ViewItems of the Arraylist are inflated
     {
-        ArrayList<ListItem> Items= new ArrayList<ListItem>();
+        ArrayList<ListItem> PostsArrayList= new ArrayList<ListItem>();
 
 
-        MyCustomAdapter(ArrayList<ListItem> Items ) {
-            this.Items=Items;
+        MyCustomAdapter(ArrayList<ListItem> PostsArrayList ) {
+            this.PostsArrayList=PostsArrayList;
         }
 
         @Override
         public int getCount() {
-            return Items.size();
+            return PostsArrayList.size();
         }
 
         @Override
         public String getItem(int position) {
-            return Items.get(position).Name;
+            return PostsArrayList.get(position).Name;
         }
 
         @Override
@@ -204,10 +201,10 @@ public class MainActivity extends AppCompatActivity {
 
 
             //get the data from Items and put it in the right places
-            txtname.setText(Items.get(i).Name);
-            txtdes.setText(Items.get(i).Desc);
-            txtuser.setText(Items.get(i).User);
-            txtdiscreption.setText(Items.get(i).Discription);
+            txtname.setText(PostsArrayList.get(i).Name);
+            txtdes.setText(PostsArrayList.get(i).Desc);
+            txtuser.setText(PostsArrayList.get(i).User);
+            txtdiscreption.setText(PostsArrayList.get(i).Discription);
 
             return view1;
 
