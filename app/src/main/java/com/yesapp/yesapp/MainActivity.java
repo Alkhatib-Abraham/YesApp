b6653f4ba31372a9075f34b0412f80347a67e48a
Abraham <alkhatib.abraham@gmail.com>
2018-08-11 20:32:47 +0200

    Splash Screen Added

--- app/src/main/java/com/yesapp/yesapp/MainActivity.java
+++ app/src/main/java/com/yesapp/yesapp/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
                 finish();
-                
+
 
             }
             }
